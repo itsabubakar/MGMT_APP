3ca03fb7f00c4abb6a88580ceafc8e5f72e70e93
Sadiq <seeker99157@gmail.com>
2022-11-17 18:09:04 +0100

    added client side

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+import Clients from "./components/Clients"
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+})
+
+const App = () => {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
+  )
+}
+export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <nav className="navbar bg-light mb-4 p-0">
+            <div className="container">
+                <a href="" className="navbar-brand">
+                    <div className="d-flex">
+                        <h2>Project MGMT</h2>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    )
+}
+export default Header
