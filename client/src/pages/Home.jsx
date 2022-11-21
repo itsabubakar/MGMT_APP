628684b7c7dbf4c6911f479a4dc8635cd57020b2
Sadiq <seeker99157@gmail.com>
2022-11-21 13:20:29 +0100

    final commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Clients from "../components/Clients"
 import AddClientModal from "../components/AddClientModal"
+import AddProjectModal from '../components/AddProjectModal'
 import Projects from "../components/Projects"
+
+
 
 const Home = () => {
     return (
         <>
-            <div className="d-flex-gap-3 mb-4"><AddClientModal /></div>
+            <div className="d-flex gap-3 mb-4">
+                <AddClientModal />
+                <AddProjectModal />
+            </div>
             <Projects />
             <hr />
             <Clients />
